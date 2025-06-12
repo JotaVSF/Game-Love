75a4280ce98c5aebcad720eac18b1263db0a7bf5
João Vitor S Ferreira <joaovdesouzaf@gmail.com>
2025-06-12 17:54:52 -0300

    Atualizar o QRcodeTrofeu.py

--- game❤/QRcodeTrofeu.py
+++ game❤/QRcodeTrofeu.py
@@ -2,7 +2,7 @@ import qrcode
 import os
 
 caminho = os.path.join(os.getcwd(), "qr_code_youtube.png")
-dado = 'https://www.youtube.com/watch?v=6EEW-9NDM5k'
+dado = 'https://m.youtube.com/shorts/l5jI54nFrko'
 img = qrcode.make(dado)
 img.save(caminho)
 print(f"QR Code salvo em: {caminho}")
